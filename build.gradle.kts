6e0658650e3b208f2a72a6de2dfed3235d95c37f
Alien <ztiany3@gmail.com>
2024-05-06 18:44:43 +0800

    Migrate to kotlin dsl (done).

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    alias(libs.plugins.app.common.library)
+}
+
+android {
+    namespace = "com.android.base.core"
+}
+
+dependencies {
+    // androidx
+    api (libs.androidx.annotations)
+    api (libs.androidx.arch.runtime)
+    api (libs.androidx.arch.common)
+    api (libs.androidx.lifecycle.common)
+    api (libs.androidx.lifecycle.common.java8)
+
+    api (libs.androidx.lifecycle.runtime.ktx)
+    api (libs.androidx.lifecycle.livedata.core)
+    api (libs.androidx.lifecycle.livedata.ktx)
+    api (libs.androidx.lifecycle.viewmodel.ktx)
+    api (libs.androidx.lifecycle.reactivestreams)
+    api (libs.androidx.lifecycle.process)
+
+    // kotlin
+    api (libs.kotlin.stdlib)
+    api (libs.kotlin.reflect)
+    api (libs.kotlinx.coroutines)
+    api (libs.kotlinx.coroutines.android)
+
+    // log
+    api (libs.jakewharton.timber)
+}
